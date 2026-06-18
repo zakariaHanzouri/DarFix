a10c75228796b7e2355eb3579c16308a39458ba9
zakariaHanzouri <elhenzo101@gmail.com>
2026-06-18 20:56:29 +0200

    feat(service): add search filter and pagination

--- backend/app/Http/Controllers/ServiceController.php
+++ backend/app/Http/Controllers/ServiceController.php
@@ -6,86 +6,111 @@ use App\Http\Requests\StoreServiceRequest;
 use App\Http\Resources\ServiceResource;
 use App\Models\Service;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 
 class ServiceController extends Controller
 {
-    public function index(){
-        $services=Service::with('category','artisan')->paginate(10);
-        
-        return response()->json([
-            'services'=>ServiceResource::collection($services)
-        ]);
-    }
+    public function index(Request $request)
+    {
 
+        $query = Service::query();
 
-    public function store(StoreServiceRequest $request){
-
-        $this->authorize('create',Service::class);
-
-        $data=$request->validated();
-        $user=auth()->user();
-
-        if ($user->role->name==='admin') {
-            $data['artisan_id']=$request->artisan_id;
-        }else if($user->role->name==='artisan'){
-            $data['artisan_id']=$user->id;
+        if ($request->filled('search')) {
+            $search = $request->search;
+            $query->where('title', 'LIKE', "%{$search}%");
         }
 
-        $service=Service::create($data);
+        if ($request->filled('category_id')) {
+            $category_id = $request->category_id;
+            $query->where('category_id', $category_id);
+        }
+
+        $services = $query->with('category', 'artisan')->paginate(1);
 
         return response()->json([
-            'service'=>new ServiceResource($service),
-            'message'=>'Service created successfully'
+            'services' => ServiceResource::collection($services),
+            'pagination' => [
+                'current_page' => $services->currentPage(),
+                'last_page' => $services->lastPage(),
+                'per_page' => $services->perPage(),
+                'total' => $services->total(),
+            ]
+        ]);
+    }
+
+
+    public function store(StoreServiceRequest $request)
+    {
+
+        $this->authorize('create', Service::class);
+
+        $data = $request->validated();
+        $user = auth()->user();
+
+        if ($user->role->name === 'admin') {
+            $data['artisan_id'] = $request->artisan_id;
+        } else if ($user->role->name === 'artisan') {
+            $data['artisan_id'] = $user->id;
+        }
+
+        $service = Service::create($data);
+
+        return response()->json([
+            'service' => new ServiceResource($service),
+            'message' => 'Service created successfully'
         ]);
 
     }
 
 
-    public function show(Service $service){
+    public function show(Service $service)
+    {
 
-        $service->load('artisan','category');
+        $service->load('artisan', 'category');
 
         return response()->json([
-            'service'=> new ServiceResource($service)
+            'service' => new ServiceResource($service)
         ]);
 
     }
 
 
-    public function update(StoreServiceRequest $request,Service $service){
+    public function update(StoreServiceRequest $request, Service $service)
+    {
 
-        $this->authorize('update',$service);
+        $this->authorize('update', $service);
 
-        $updatedData=$request->validated();
+        $updatedData = $request->validated();
 
-        $user=auth()->user();
+        $user = auth()->user();
 
-        if ($user->role->name ==='admin') {
-            $updatedData['artisan_id'] =  $request->artisan_id;
-        }else if ($user->role->name ==='artisan'){
-            $updatedData['artisan_id']=$user->id;
+        if ($user->role->name === 'admin') {
+            $updatedData['artisan_id'] = $request->artisan_id;
+        } else if ($user->role->name === 'artisan') {
+            $updatedData['artisan_id'] = $user->id;
         }
 
 
         $service->update($updatedData);
 
         return response()->json([
-            'service'=>new ServiceResource($service),
-            'message'=>'Service updated successfully'
+            'service' => new ServiceResource($service),
+            'message' => 'Service updated successfully'
         ]);
 
     }
 
-    public function destroy(Service $service){
+    public function destroy(Service $service)
+    {
 
-        $this->authorize('delete',$service);
+        $this->authorize('delete', $service);
 
         $service->delete();
 
         return response()->json([
-            'success'=>true,
-            'message'=>'Service Deleted successfully ',
+            'success' => true,
+            'message' => 'Service Deleted successfully ',
         ]);
 
     }
